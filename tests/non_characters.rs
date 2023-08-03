c0b629628638c3b4edc43486271e01fdb7cc6027
Dustin J. Mitchell <djmitche@google.com>
2023-08-03 20:49:19 +0000

    Test for literal non-character

--- tests/non_characters.rs
+++ tests/non_characters.rs
@@ -6,11 +6,18 @@ extern crate serde_json_lenient;
 use serde_json_lenient::{from_str, Value};
 
 #[test]
-fn test_non_character() {
+fn test_escaped_non_character() {
     let s = r#"
     {
         "key": "value\ufdef\uffff"
     }"#;
     let value: Value = from_str(s).unwrap();
     assert_eq!(value, json!({"key": "value\u{fffd}\u{fffd}"}));
+}
+
+#[test]
+fn test_literal_non_character() {
+    let s = "{\"key\": \"value\u{fdd0}\"}";
+    let value: Value = from_str(s).unwrap();
+    assert_eq!(value, json!({"key": "value\u{fffd}"}));
 }
